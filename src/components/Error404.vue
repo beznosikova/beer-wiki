1b1d8a9d9e942c0af71a3a74140d45460aeedd22
Tetiana Beznosikova <beznosikovat@gmail.com>
2023-06-25 18:45:07 +0200

    Add Error404 component

---
+++ src/components/Error404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div role="alert">
+    <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+      404 - error
+    </div>
+    <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+      <p>Ta strona nie istnieje, przejdź do <router-link to="/">strony głównej</router-link></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error404",
+};
+</script>
+
